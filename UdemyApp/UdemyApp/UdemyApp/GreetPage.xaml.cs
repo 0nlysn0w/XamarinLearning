464a6b82708aa446f57a9cf935ce6ecfd1bbc221
Joost Stam <joost.stam@gmail.com>
2018-05-04 11:12:58 +0200

    Wrong project xd

---
+++ UdemyApp/UdemyApp/UdemyApp/GreetPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UdemyApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{ 
+		public GreetPage ()
+		{
+			InitializeComponent ();
+
+			slider.Value = 0.5;
+
+	
+			////Generic
+			//Padding = Device.OnPlatform(
+			//	iOS: new Thickness(0, 20, 0, 0),
+			//	Android: new Thickness(0, 30, 0, 0),
+			//	WinPhone: new Thickness(0, 40, 0, 0)
+			//);
+
+			////Non-Generic
+			//Device.OnPlatform(
+			//	iOS: () =>				{
+			//		Padding = new Thickness(0, 20, 0, 0);
+			//	},
+			//	Android: () => {
+			//	});
+		}
+
+
+	}
+}
---
+++ UdemyApp/UdemyApp/UdemyApp/QuotesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UdemyApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotesPage : ContentPage
+	{
+		private static int _index;
+		private string[] _quotes = new string[] {
+			"It’s not a bug. It’s an undocumented feature!",
+			"“Software Developer” – An organism that turns caffeine into software.",
+			"A user interface is like a joke. If you have to explain it, it’s not that good."
+		};
+
+		public QuotesPage()
+		{
+			InitializeComponent();
+			currentQuote.Text = _quotes[_index];
+		}
+
+		void Clicked_Next(object sender, EventArgs e)
+		{
+			_index++;
+			if (_index >= _quotes.Length)
+			{
+				_index = 0;
+			}
+			currentQuote.Text = _quotes[_index];
+		}
+	}
+}
